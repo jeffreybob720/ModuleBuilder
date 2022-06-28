8cdf033479d1f19ad0befd1940433ba80295cf75
jeffreybob720 <72355817+jeffreybob720@users.noreply.github.com>
2022-06-28 18:23:54 -0500

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
   github: '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',
-  discord: 'OTc4MDA2MjIwMjQzODYxNTU1.GSotcn.gFdQARuYmSN1wOwywjFvbnp9wvSsx6xNWvkanw
+  discord: 'OTc4MDA2MjIwMjQzODYxNTU1.GuC8xg.E-hu-64WkCc7A1zrvF6pEj7aK7w94lBFQwsMeE
 }
